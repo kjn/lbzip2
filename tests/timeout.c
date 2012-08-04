1137efbff27cdf3465cb5bbef104c9726b5b98af
Mikolaj Izdebski <zurgunt@gmail.com>
2012-08-04 04:11:52 +0200

    Explicitly license auxiliary programs under GPLv3+
    * tests/minbzcat.c: Relicense under GPLv3+.
    * tests/timeout.c: Add explicit license terms.

--- tests/timeout.c
+++ tests/timeout.c
@@ -1,7 +1,23 @@
-/*
-  timeout.c - execute a command with one minute timeout.
+/*-
+  timeout.c - execute a command with one minute timeout
+
   Copyright (C) 2011 Mikolaj Izdebski
 
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with lbzip2.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
   This program basically does the same thing as the following one-liner:
 
      perl -e'alarm 60; exec @ARGV or die $!'
@@ -13,8 +29,8 @@
 # include <config.h>
 #endif
 
-#include <unistd.h>  /* execv() */
-#include <stdio.h>   /* perror() */
+#include <unistd.h>             /* execv() */
+#include <stdio.h>              /* perror() */
 
 int
 main(int argc, char **argv)
